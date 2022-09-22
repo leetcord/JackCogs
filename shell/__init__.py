598f39b5483a844bea1acc64112026b58ccae2ae
Leet <36166244+leetfin@users.noreply.github.com>
2022-09-22 14:58:08 -0400

    dpy2 compat

--- shell/__init__.py
+++ shell/__init__.py
@@ -26,4 +26,4 @@ with open(Path(__file__).parent / "info.json") as fp:
 async def setup(bot: Red) -> None:
     cog = Shell(bot)
     await cog.initialize()
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
